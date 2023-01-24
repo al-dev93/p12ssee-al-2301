0608a3cd2df676949b260ba09b9fdde5a6559adf
A_Larose <larose.alain@gmail.com>
2023-01-25 00:39:09 +0100

    load user id in url and fecth data in H1

--- sport-see-frontend/src/pages/Profil/index.jsx
+++ sport-see-frontend/src/pages/Profil/index.jsx
@@ -1,10 +1,23 @@
 import "./style.css";
+import { useParams } from "react-router-dom";
+import { USER_MAIN_DATA } from "../../utils/urlMockData";
+import useFetchData from "../../services/api/useFetchData";
 
 const Profil = () => {
+  const { userId } = useParams();
+  const { usersData } = useFetchData(USER_MAIN_DATA);
+
   return (
     <div className="profilWrapper">
       <header>
-        <h1>Bonjour</h1>
+        <h1>
+          Bonjour{" "}
+          <strong>
+            {usersData &&
+              usersData.find((user) => `${user.id}` === userId).userInfos
+                .firstName}
+          </strong>
+        </h1>
         <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
       </header>
     </div>
